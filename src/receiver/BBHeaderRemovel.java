044696c2b3eefb225cd9ddd48501d33db90d2938
sumanan <sumanan@mit.edu>
2017-02-27 15:51:02 +0800

    BBHeaderRemovel.java formatted
    using eclipse's default formatting options. I just pressed Ctrl+Shift+F and then
    Ctrl+Shift+O. No any other changes were made.

--- src/receiver/BBHeaderRemovel.java
+++ src/receiver/BBHeaderRemovel.java
@@ -1,44 +1,40 @@
 package receiver;
 
 import edu.mit.streamjit.api.Filter;
+import edu.mit.streamjit.api.Pipeline;
 import edu.mit.streamjit.api.RoundrobinJoiner;
 import edu.mit.streamjit.api.RoundrobinSplitter;
 import edu.mit.streamjit.api.Splitjoin;
-import receiver.FEC_Frame;
 
-public class BBHeaderRemovel extends edu.mit.streamjit.api.Pipeline<FEC_Frame, FEC_Frame>{
-	
+public class BBHeaderRemovel extends Pipeline<FEC_Frame, FEC_Frame> {
+
 	@SuppressWarnings("unchecked")
-	public BBHeaderRemovel(){
-//		this.add(new BitsFromFEC());
-		this.add(
-				new Splitjoin<FEC_Frame,FEC_Frame>(
-							new RoundrobinSplitter<FEC_Frame>(1),
-							new RoundrobinJoiner<FEC_Frame>(1),
-							new BitsFromFEC(), new BitsFromFEC()
-							
-				)
-		);
+	public BBHeaderRemovel() {
+		// this.add(new BitsFromFEC());
+		this.add(new Splitjoin<FEC_Frame, FEC_Frame>(new RoundrobinSplitter<FEC_Frame>(1),
+				new RoundrobinJoiner<FEC_Frame>(1), new BitsFromFEC(), new BitsFromFEC()
+
+		));
 	}
-	
+
 	private static class BitsFromFEC extends Filter<FEC_Frame, FEC_Frame> {
-		
+
 		public BitsFromFEC() {
-			super(1,1);  // 32208-80
+			super(1, 1); // 32208-80
 		}
 
 		@Override
 		public void work() {
-//			System.out.println("Remove BB header and push data only-------------");
+			// System.out.println("Remove BB header and push data
+			// only-------------");
 			FEC_Frame current_frame = pop();
 			boolean[] datawithbb = current_frame.getFEC_Data();
 			boolean[] data = new boolean[64720];
-			
-			
+
 			for (int i = 0; i < data.length; i++) {
-				data[i] = datawithbb[i+80];
+				data[i] = datawithbb[i + 80];
 			}
-			
+
 			FEC_Frame frame = new FEC_Frame(data);
 			push(frame);
 		}
