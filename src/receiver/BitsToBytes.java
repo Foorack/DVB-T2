13b4428bff408f2773c8703b4d8b63187d26426d
sumanan <sumanan@mit.edu>
2017-02-27 15:58:43 +0800

    BitsToBytes.java formatted
    using eclipse's default formatting options. I just pressed Ctrl+Shift+F and then
    Ctrl+Shift+O. No any other changes were made.

--- src/receiver/BitsToBytes.java
+++ src/receiver/BitsToBytes.java
@@ -1,13 +1,16 @@
 package receiver;
 
-public class BitsToBytes extends edu.mit.streamjit.api.Pipeline<Byte, Byte>{
-	
-	public BitsToBytes(){
+import edu.mit.streamjit.api.Filter;
+import edu.mit.streamjit.api.Pipeline;
+
+public class BitsToBytes extends Pipeline<Byte, Byte> {
+
+	public BitsToBytes() {
 		this.add(new MakeBytes());
 	}
-	
-	private static class MakeBytes extends edu.mit.streamjit.api.Filter<Byte, Byte> {
-		
+
+	private static class MakeBytes extends Filter<Byte, Byte> {
+
 		public MakeBytes() {
 			super(8, 1);
 		}
@@ -17,19 +20,18 @@ public class BitsToBytes extends edu.mit.streamjit.api.Pipeline<Byte, Byte>{
 			byte[] data_array = new byte[8];
 			StringBuilder builder = new StringBuilder();
 			byte byte_value;
-							
+
 			for (int i = 0; i < 8; i++) {
 				data_array[i] = pop();
 				builder.append(data_array[i]);
 			}
-			byte_value = (byte) Integer.parseInt(builder.toString(),2);
-//			System.out.println(builder.toString()+"      "+byte_value);
-//			WriteToFile writer = new WriteToFile();
-//			writer.appendToFile(byte_value, path);
-//			System.out.println(byte_value+" ");
-			push(byte_value);		
-			
-		}
+			byte_value = (byte) Integer.parseInt(builder.toString(), 2);
+			// System.out.println(builder.toString()+" "+byte_value);
+			// WriteToFile writer = new WriteToFile();
+			// writer.appendToFile(byte_value, path);
+			// System.out.println(byte_value+" ");
+			push(byte_value);
 
+		}
 	}
 }
